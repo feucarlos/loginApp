f2fbdc514c4e8adf078b0e2d31709e121baeb1a3
Carlos Gutierrez <feucarlos@gmail.com>
2020-08-18 22:37:10 -0500

    loading con Swal

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel();
 
-  constructor( private auth: AuthService ) { }
+  constructor( private auth: AuthService, private router: Router ) { }
 
   ngOnInit(): void {
   }
@@ -21,11 +23,27 @@ export class LoginComponent implements OnInit {
 
     if ( form.invalid ) { return; }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.login( this.usuario )
       .subscribe( resp => {
         console.log( resp );
+        Swal.close();
+        this.router.navigateByUrl('/home');
       }, (err) => {
         console.log( err.error.error.message );
+        Swal.fire({
+          allowOutsideClick: true,
+          type: 'error',
+          title: 'Error en credenciales',
+          text: err.error.error.message
+        });
+    
       });
   }
 
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { UsuarioModel } from '../../models/usuario.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -12,7 +14,7 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor( private auth: AuthService ) { }
+  constructor( private auth: AuthService, private router: Router ) { }
 
   ngOnInit(): void {
     this.usuario = new UsuarioModel();
@@ -23,12 +25,25 @@ export class RegistroComponent implements OnInit {
 
     if ( form.invalid ) { return; }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.nuevoUsuario ( this.usuario )
       .subscribe( resp => {
         console.log( resp );
+        Swal.close();
+        this.router.navigateByUrl('/home');
       }, (err) => {
         console.log( err.error.error.message );
-        
+        Swal.fire({
+          allowOutsideClick: true,
+          type: 'error',
+          text: err.error.error.message
+        });
       });
 
   }
